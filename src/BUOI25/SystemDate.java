a7c7c74a02832cdc58c846b8aeeb5e642a4dbd9a
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-02 20:54:09 +0700

    add file

---
+++ src/BUOI25/SystemDate.java
@@ -0,0 +1,9 @@
+package BUOI25;
+
+import java.util.Date;
+public class SystemDate {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
