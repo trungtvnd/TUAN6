db09026909029aff9a280a2529b248f41239f9c8
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-02 21:01:32 +0700

    add file

---
+++ src/BUOI25/variable_type.java
@@ -0,0 +1,18 @@
+package BUOI25;
+
+public class variable_type {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i la: " + i);
+        System.out.println("f la: " + f);
+        System.out.println("d la: " + d);
+        System.out.println("b la: " + b);
+        System.out.println("s la: " + s);
+        System.out.println("c la: " + c);
+    }
+}
