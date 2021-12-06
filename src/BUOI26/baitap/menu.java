2c21f504ee8fc78920c7025ac1094f34dbc267c3
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-06 11:43:52 +0700

    add file

--- src/BUOI26/baitap/menu.java
+++ src/BUOI26/baitap/menu.java
@@ -3,6 +3,91 @@ package BUOI26.baitap;
 import java.util.Scanner;
 
 public class menu {
+    public static void Rectangle() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter width");
+        int width = input.nextInt();
+        System.out.println("Enter height");
+        int height = input.nextInt();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Triangle_TopLeft() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter height:");
+        int height = input.nextInt();
+        for (int i = 0; i <= height; i++) {
+            for (int j = height; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void Triangle_Topright() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter height:");
+        int height = input.nextInt();
+        for (int i = 0; i <= height; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 10; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Triangle_BottomRight() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter height:");
+        int height = input.nextInt();
+        for (int i = 0; i <= height; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Triangle_BottomLeft() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter height:");
+        int height = input.nextInt();
+        for (int i = 0; i <= height; i++) {
+            for (int j = height; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void isosceles_Triangle() {
+        System.out.println("Enter height");
+        Scanner input = new Scanner(System.in);
+        int height = input.nextInt();
+        for (int i = 0; i <= height; i++) {
+            for (int j = height; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int choice;
@@ -16,69 +101,44 @@ public class menu {
             System.out.print("Would you like to choose something");
             choice = input.nextInt();
             if (choice == 1) {
-                System.out.println("***********");
-                System.out.println("***********");
-                System.out.println("***********");
-                System.out.println("***********");
+                Rectangle();
+
             } else if (choice == 2) {
                 int choice1;
-                System.out.print("Would you like to choose something");
-                choice1 = input.nextInt();
+
                 System.out.println("1. The corner is square at: top-left");
                 System.out.println("2. The corner is square at: top-right");
-                System.out.println("3. The corner is square at: botton-left");
-                System.out.println("4. The corner is square at: botton-right");
+                System.out.println("3. The corner is square at: botton-right");
+                System.out.println("4. The corner is square at: botton-left");
                 System.out.println("0. Exit");
+                System.out.println("Would you like to choose something");
+                choice1 = input.nextInt();
+
                 switch (choice1) {
-                    case 3:
-                        for (int i = 0; i <= 10; i++) {
-                            for (int j = 0; j <= i; j++) {
-                                System.out.print("*");
-                            }
-                            System.out.println();
-                        }
                     case 1:
-                        for (int i = 0; i <= 10; i++) {
-                            for (int j = 10; j >= i; j--) {
-                                System.out.print("*");
-                            }
-                            System.out.println();
-                        }
+                        Triangle_TopLeft();
+                        break;
                     case 2:
-                        for (int i = 0; i <= 10; i++) {
-                            for (int j = 0; j <= i; j++) {
-                                System.out.print(" ");
-                            }
-                            for (int j = 10; j >= i; j--) {
-                                System.out.print("*");
-                            }
-                            System.out.println();
-                        }
+                        Triangle_Topright();
+                        break;
+                    case 3:
+                        Triangle_BottomRight();
+                        break;
                     case 4:
-                        for (int i = 0; i <= 10; i++) {
-                            for (int j = 10; j >= i; j--) {
-                                System.out.print(" ");
-                            }
-                            for (int j = 0; j <= i; j++) {
-                                System.out.print("*");
-                            }
-                            System.out.println();
-                        }
+                        Triangle_BottomLeft();
+                        break;
+                    case 0:
+                        System.exit(0);
+                    default:
+                        System.out.println("No Choice");
                 }
             } else if (choice == 3) {
-                for (int r = 0; r <= 9; r++) {
-                    for (int sp = 9 - r + 1; sp >= 1; sp--) {
-                        System.out.print(" ");
-                    }
-                    for (int c = 1; c <= r; c++) {
-                        System.out.print("*");
-                    }
-                    for (int c = r - 1; c >= 1; c--) {
-                        System.out.print("*");
-                    }
-                    System.out.println();
-                }
+                isosceles_Triangle();
+            }
+            else if (choice == 4) {
+                System.exit(0);
             }
         }
     }
 }
+
---
+++ src/BUOI26/thuchanh/chuyenDoiNhietDo.java
@@ -0,0 +1,19 @@
+package BUOI26.thuchanh;
+
+public class chuyenDoiNhietDo {
+    public static void main(String[] args) {
+        System.out.println("Chuyen từ F sang C: " + FtoC(50));
+        System.out.println("Chuyen từ C sang F: " + CtoF(28));
+    }
+
+    public static double FtoC(double fahrenheit) {
+        double celcius;
+    celcius = (5.0 / 9) *(fahrenheit -32);
+    return celcius;
+    }
+    public static double CtoF(double celcius) {
+        double fahrenheit;
+        fahrenheit = (celcius * 9 / 5) +32;
+        return fahrenheit;
+    }
+}
