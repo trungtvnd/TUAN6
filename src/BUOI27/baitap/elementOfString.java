a6a11d7ee6977234b958d2b4fc80085f87bd5481
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-06 22:34:02 +0700

    add file

---
+++ src/BUOI27/baitap/elementOfString.java
@@ -0,0 +1,28 @@
+package BUOI27.baitap;
+
+import java.util.Scanner;
+
+public class elementOfString {
+    public static void main(String[] args) {
+        String chuoi;
+        char kyTu = 'a';
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào chuỗi bất kỳ: ");
+        chuoi = scanner.nextLine();
+
+        // duyệt từ đầu đến cuối chuỗi
+        for (int i = 0; i < chuoi.length(); i++) {
+            // Nếu ký tự tại vị trí thứ i bằng 'a' thì tăng count lên 1
+            if (chuoi.charAt(i) == kyTu) {
+                count++;
+            }
+        }
+
+        System.out.println("Số lần xuất hiện của ký tự " + kyTu +
+                " trong chuỗi " + chuoi + " = " + count);
+    }
+
+
+}
