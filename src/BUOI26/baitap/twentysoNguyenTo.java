36689d34107adf9d77dc6eef30f64271d09a392c
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-06 16:40:24 +0700

    add file

--- src/BUOI26/baitap/twentysoNguyenTo.java
+++ src/BUOI26/baitap/twentysoNguyenTo.java
@@ -1,22 +1,46 @@
 package BUOI26.baitap;
 
+import java.util.Scanner;
+
 public class twentysoNguyenTo {
     public static void main(String[] args) {
+//        int count = 0;
+//        int N = 2;
+//        boolean flag = true;
+//        while (count <= 20){
+//            for (int i = 2; i <= Math.sqrt(N);i ++) {
+//                if(N % i == 0){
+//                    flag = false;
+//                    break;
+//                }
+//            }if(flag){
+//                count++;
+//                System.out.println(N);
+//            }
+//            flag = true;
+//            N++;
+//        }
+        int number = 2;
         int count = 0;
-        int N = 2;
-        boolean flag = true;
-        while (count <= 20){
-            for (int i = 2; i <= Math.sqrt(N);i ++) {
-                if(N % i == 0){
-                    flag = false;
-                    break;
-                }
-            }if(flag){
+        while (count <=20){
+            if(isPrime(number)){
+                System.out.println(number);
                 count++;
-                System.out.println(N);
             }
-            flag = true;
-            N++;
+            number++;
         }
+    }
+
+    static boolean isPrime(int number){
+
+        if(number < 2){
+            return false;
+        }else{
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if(number % i == 0){
+                    return false;
+                }
+            }
+        }return true;
     }
 }
