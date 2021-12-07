86570f07724a19951efbdcfea12e2db7eea84199
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-07 08:30:19 +0700

    add file

--- src/BUOI27/baitap/elementOfString.java
+++ src/BUOI27/baitap/elementOfString.java
@@ -4,23 +4,20 @@ import java.util.Scanner;
 
 public class elementOfString {
     public static void main(String[] args) {
-        String chuoi;
+        String string;
         char kyTu = 'a';
         int count = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Nhập vào chuỗi bất kỳ: ");
-        chuoi = scanner.nextLine();
-
-        // duyệt từ đầu đến cuối chuỗi
-        for (int i = 0; i < chuoi.length(); i++) {
-            // Nếu ký tự tại vị trí thứ i bằng 'a' thì tăng count lên 1
-            if (chuoi.charAt(i) == kyTu) {
+        string = scanner.nextLine();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == kyTu) {
                 count++;
             }
         }
         System.out.println("Số lần xuất hiện của ký tự " + kyTu +
-                " trong chuỗi " + chuoi + " = " + count);
+                " trong chuỗi " + string + " = " + count);
     }
 
 
