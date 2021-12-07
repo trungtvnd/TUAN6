83717fe7d5f743b852f59245523a24c6c967bf0d
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-07 08:19:03 +0700

    add file

--- src/BUOI27/baitap/addElement.java
+++ src/BUOI27/baitap/addElement.java
@@ -5,29 +5,24 @@ import java.util.Scanner;
 
 public class addElement {
     public static void main(String[] args) {
-        int[] array = {1, 2, 4, 5, 6, 6, 7, 8};
-        int number, index;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter number");
-        number = input.nextInt();
-        System.out.println("Enter index");
-        index = input.nextInt();
-        if (index < 0 || index > array.length - 1) {
-            System.out.println("Không chèn được phần tử vào mảng");
-        } else {
-            array = addElement(array, index, number);
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int n = arr.length;
+        int index = 3;
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        int[] newArr = new int[n + 1];
+        int j = 0;
+
+        for (int i = 0; i < newArr.length; i++) {
+            if (i == index) {
+                newArr[i] = 7;
+            } else {
+                newArr[i] = arr[j];
+                j++;
+            }
         }
-        System.out.println(Arrays.toString(array));
+        newArr[index] = 7;
+
+        System.out.println("Array after adding value: " + Arrays.toString(newArr));
     }
 
-    static int[] addElement(int[] array, int index, int number) {
-        int[] newArray = new int[array.length + 1];
-        for (int i = 0; i < index; i++) {
-            newArray[i] = array[i];
-        }
-        for (int i = index; i < array.length + 1; i++) {
-            newArray[index] = number;
-        }
-        return newArray;
-    }
 }
--- src/BUOI27/baitap/gopMang.java
+++ src/BUOI27/baitap/gopMang.java
@@ -4,28 +4,26 @@ import java.util.Arrays;
 
 public class gopMang {
     public static void main(String[] args) {
-        //khai báo và khởi tạo giá trị cho các phần tử của hai mảng array1 và array2
+
         int[] array1 = {1, 2, 3};
         int[] array2 = {4, 5, 6};
-        //biến length sẽ bằng độ dài của hai mảng cộng lại
+
         int length = array1.length + array2.length;
-        //Bây giờ khai báo thêm một mảng mới result để lưu trữ hai mảng cần nối, với độ dài bằng length
+
         int[] result = new int[length];
         int pos = 0;
-        //sử dụng vòng lặp for để lưu các phần tử trong mảng array1 vào mảng result
+
         for (int element : array1) {
             result[pos] = element;
             pos++;
         }
-        //tương tự như vậy cho mảng hai
+
         for (int element : array2) {
             result[pos] = element;
             pos++;
         }
-        //sau khi nối xong ta chỉ cần hiển thị mảng result ra màn hình
+
         System.out.println(Arrays.toString(result));
 
-        System.out.println("\n----------------------------------");
-        System.out.println("Chương trình này được đăng tại Freetuts.net");
     }
 }
