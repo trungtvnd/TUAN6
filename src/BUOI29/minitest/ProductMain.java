e5a1a23233ae4b2dd5f6190afa748f3f7d17414a
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-09 16:11:56 +0700

    add file

--- src/BUOI29/minitest/ProductMain.java
+++ src/BUOI29/minitest/ProductMain.java
@@ -5,34 +5,34 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class ProductMain {
-//    public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("Nhập số lượng sản phẩm");
-//        int n = input.nextInt();
-//        System.out.println("Nhập mã:");
-//        String codeProduct = input.nextLine();
-//        input.nextLine();
-//        System.out.println("Nhap name");
-//        String name = input.nextLine();
-//        System.out.println("Nhập giá");
-//        double price = input.nextDouble();
-//        input.nextLine();
-//        System.out.println("Nhập loại");
-//        String type = input.nextLine();
-//        System.out.println("Nhập loại tiền");
-//        String currentcy = input.nextLine();
-//
-//        Product[] productList = new Product[n];
-//        for (int i = 0; i < productList.length; i++) {
-//            productList[i] = new Product(codeProduct, name, price, type, currentcy);
-//        }
-//        for (Product product: productList) {
-//            System.out.println(product);
-//        }
-//
-//    }
+    public static void input() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập số lượng sản phẩm");
+        int n = input.nextInt();
+        System.out.println("Nhập mã:");
+        String codeProduct = input.nextLine();
+        input.nextLine();
+        System.out.println("Nhap name");
+        String name = input.nextLine();
+        System.out.println("Nhập giá");
+        double price = input.nextDouble();
+        input.nextLine();
+        System.out.println("Nhập loại");
+        String type = input.nextLine();
+        System.out.println("Nhập loại tiền");
+        String currentcy = input.nextLine();
+
+        Product[] productList = new Product[n];
+        for (int i = 0; i < productList.length; i++) {
+            productList[i] = new Product(codeProduct, name, price, type, currentcy);
+        }
+        for (Product product: productList) {
+            System.out.println(product);
+        }
+
+    }
 public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+//    Scanner input = new Scanner(System.in);
     Product[] products = new Product[3];
     Product product1 = new Product("abc123","Bike", 10000, "Mới", "USD" );
     Product product2 = new Product("abc12345","Bicycle", 15000, "Cũ", "USD" );
@@ -44,10 +44,10 @@ public static void main(String[] args) {
     for (Product product:products) {
         System.out.println(product);
     }
+
+
     System.out.println(Arrays.toString(products));
 
     System.out.println("Tổng giá là: " + Product.total);
 }
-
-
 }
